0a3cc98f5847d0f0e8283314fabeeb7e8fe7bc59
ColossalCoin <abelcarvajal18@gmail.com>
2026-03-22 18:37:42 -0600

    Add `target_runner.bat` for executing `irace_wrapper.py` with hyperparameters, enabling smoother integration with irace

---
+++ irace/target_runner.bat
@@ -0,0 +1,27 @@
+@echo off
+set CONFIG_ID=%1
+set INSTANCE_ID=%2
+set SEED=%3
+set INSTANCE=%4
+
+:: Desplazamos los primeros 4 argumentos que son fijos de irace
+shift
+shift
+shift
+shift
+
+:: Recopilamos todos los hiperparámetros restantes
+set "REST_ARGS="
+:loop
+if "%~1"=="" goto run
+set "REST_ARGS=%REST_ARGS% %1"
+shift
+goto loop
+
+:run
+.\envs\thesis_env\Scripts\python.exe irace_wrapper.py --inst %INSTANCE% --seed %SEED% %REST_ARGS%
+
+:: Capturamos errores silenciosamente para que irace no se confunda
+if %ERRORLEVEL% NEQ 0 (
+    echo inf
+)
